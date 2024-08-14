8c3ac3a6d5f3015dfa5f266925e26a48b24fc633
Laura Gabriel <75498044+LauraGabrielSantes@users.noreply.github.com>
2024-08-13 18:43:41 -0600

    primer commit

---
+++ back/src/main/kotlin/mx/uam/coordinacion/CoordinacionApplication.kt
@@ -0,0 +1,11 @@
+package mx.uam.coordinacion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoordinacionApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoordinacionApplication>(*args)
+}
---
+++ back/src/test/kotlin/mx/uam/coordinacion/CoordinacionApplicationTests.kt
@@ -0,0 +1,13 @@
+package mx.uam.coordinacion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoordinacionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
